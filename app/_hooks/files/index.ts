a1486cdce06f528219c67077979a46cb6294bb6f
Dustin McCaffree <dustinnicholasmccaffree@gmail.com>
2024-05-07 17:09:17 -0500

    Update index file.

--- app/_hooks/files/index.ts
+++ app/_hooks/files/index.ts
@@ -1,3 +1,6 @@
 export { useHandleFileClick } from './use-handle-file-click';
 export { useUploadFile } from './use-upload-file';
 export { useDeleteFile } from './use-delete-file';
+export { useListFiles } from './use-list-files';
+export { useMoveFile } from './use-move-file';
+export { useRenameFile } from './use-rename-file';
