f7c80ee4508c6d2df5582c5124a14971bb3ce413
Dustin McCaffree <dustinnicholasmccaffree@gmail.com>
2024-05-10 17:38:26 -0500

    Get logout working.

--- app/_components/layout/Sidebar/SettingsMenu.tsx
+++ app/_components/layout/Sidebar/SettingsMenu.tsx
@@ -13,30 +13,32 @@ import { Menu, MenuItems, MenuItem, MenuButton } from "@headlessui/react";
 import { Fragment, use } from "react";
 import { SessionContext } from "@/app/_context/SessionContext";
 import clsx from "clsx";
+import Link from "next/link";
 
-const SettingsMenuItem = ({
+export const SettingsMenuItem = ({
   label,
-  onClick,
+  href,
   Icon,
 }: {
   label: string;
-  onClick: () => void;
+  href: string;
   Icon?: React.FC<React.SVGProps<SVGSVGElement>>;
 }) => (
   <MenuItem>
     {({ active }) => (
-      <div
-        className={clsx(
-          "flex items-center py-2 px-2 rounded-lg cursor-pointer text-sm font-medium border space-x-3",
-          active
-            ? "bg-zinc-50 text-zinc-900 border-zinc-100"
-            : "bg-white text-zinc-700 border-transparent"
-        )}
-        onClick={onClick}
-      >
-        {Icon && <Icon className="h-5 w-5" />}
-        <span>{label}</span>
-      </div>
+      <Link href={href}>
+        <div
+          className={clsx(
+            "flex items-center py-2 px-2 rounded-lg cursor-pointer text-sm font-medium border space-x-3",
+            active
+              ? "bg-zinc-50 text-zinc-900 border-zinc-100"
+              : "bg-white text-zinc-700 border-transparent"
+          )}
+        >
+          {Icon && <Icon className="h-5 w-5" />}
+          <span>{label}</span>
+        </div>
+      </Link>
     )}
   </MenuItem>
 );
@@ -83,28 +85,28 @@ export const SettingsMenu = async () => {
         <MenuItems className="bg-white border overflow-hidden rounded-xl min-w-52 border-zinc-100 shadow-md absolute bottom-full right-0 mb-1 p-1">
           <SettingsMenuItem
             label="Add a Bucket"
-            onClick={() => console.log("BUCKET")}
             Icon={PlusIcon}
+            href="/settings/buckets/new"
           />
           <SettingsMenuItem
             label="API Keys"
-            onClick={() => console.log("ACCOUNT")}
             Icon={KeyIcon}
+            href="/settings/keys"
           />
           <SettingsMenuItem
             label="Account Settings"
-            onClick={() => console.log("ACCOUNT")}
             Icon={UserCircleIcon}
+            href="/settings/account"
           />
           <SettingsMenuItem
             label="Workspace Settings"
-            onClick={() => console.log("WORKSPACE")}
             Icon={BuildingOffice2Icon}
+            href="/settings/workspace"
           />
           <SettingsMenuItem
             label="Log out"
-            onClick={() => console.log("LOG OUT")}
             Icon={ArrowRightStartOnRectangleIcon}
+            href="/api/logout"
           />
         </MenuItems>
       </Menu>
