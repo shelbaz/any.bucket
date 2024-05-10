9540357afda339bea39c19787dbb0af450d0cd16
Dustin McCaffree <dustinnicholasmccaffree@gmail.com>
2024-05-10 09:13:27 -0500

    Middleware check session.

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+import { SessionData, sessionOptions } from "./app/_lib";
 
 export const getBearerToken = (request: NextRequest) => {
     const authHeader = request.headers.get("Authorization");
@@ -31,24 +34,30 @@ export const checkBearerTokenIsValid = (request: NextRequest) => {
     return true;
 };
 
-export function middleware(request: NextRequest) {
+const validateSession = async () => {
+    const session = await getIronSession<SessionData>(cookies(), sessionOptions);
+    return session && session.isLoggedIn;
+}
+
+export async function middleware(request: NextRequest) {
     const url = request.url;
     const readonly = process.env.NEXT_PUBLIC_READONLY === "true";
+    const hasValidSession = await validateSession();
 
     if (url.includes("api/s3/objects/list")) {
-        if (!checkBearerTokenIsValid(request)) {
+        if (!hasValidSession) {
             return NextResponse.json("Unauthorized", { status: 401 });
         }
 
         return NextResponse.next();
     }
 
-    if (url.includes("api/s3") && (!checkBearerTokenIsValid(request) || readonly)) {
+    if (url.includes("api/s3") && (!hasValidSession || readonly)) {
         return NextResponse.json("Unauthorized", { status: 401 });
     }
 
     if (url.includes("api/images")) {
-        if (!checkBearerTokenIsValid(request)) {
+        if (!hasValidSession) {
             return NextResponse.json("Unauthorized", { status: 401 });
         }
 
@@ -57,10 +66,27 @@ export function middleware(request: NextRequest) {
         }
     }
 
+    if (url.includes("login") || url.includes("signup")) {
+        return NextResponse.next();
+    }
+
+    if (url === "/") {
+        return NextResponse.next();
+    }
+
+    if (url.includes("/files") || url.includes("/settings") || url.includes("/profile") || url.includes("/create")) {
+        if (!hasValidSession) {
+            const nextUrl = request.nextUrl.clone();
+            nextUrl.pathname = '/login'
+            return NextResponse.redirect(nextUrl);
+        }
+    }
+
+
     return NextResponse.next();
   }
    
   // See "Matching Paths" below to learn more
   export const config = {
-    matcher: ["/api/:path*"],
+    matcher: ["/:path*"],
   }
