4c06c996f44aab55d0f6e5233d9aad5b78f3097f
Dustin McCaffree <dustinnicholasmccaffree@gmail.com>
2024-05-13 12:14:59 -0500

    Update to use selected bucketId.

--- app/api/s3/objects/rename/route.ts
+++ app/api/s3/objects/rename/route.ts
@@ -1,52 +1,72 @@
-import { S3Client, _Object, CopyObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { Bucket, getBucketById } from "@/app/_db/bucket";
+import { getUserSession } from "@/app/_lib/session";
+import {
+  S3Client,
+  _Object,
+  CopyObjectCommand,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+import { ObjectId } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
-const renameOrMoveObject = async ({ oldKey, newKey }: { oldKey: string, newKey: string }) => {
-    const s3Url = process.env.S3_ENDPOINT;
-    const bucket = process.env.S3_BUCKET_NAME;
+const renameOrMoveObject = async ({
+  oldKey,
+  newKey,
+  bucket,
+}: {
+  oldKey: string;
+  newKey: string;
+  bucket: Bucket;
+}) => {
+  const s3Url = bucket.endpoint;
+  const name = bucket.name;
 
-    const client = new S3Client({
-        endpoint: s3Url,
-        forcePathStyle: true,
-        region: process.env.S3_REGION ?? "auto",
-        credentials: {
-            accessKeyId: process.env.S3_ACCESS_KEY_ID ?? "",
-            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY ?? "",
-        },
-    });
+  const client = new S3Client({
+    endpoint: s3Url,
+    forcePathStyle: true,
+    region: bucket.region ?? "auto",
+    credentials: {
+      accessKeyId: bucket.accessKeyId ?? "",
+      secretAccessKey: bucket.secretAccessKey ?? "",
+    },
+  });
 
-    const copyCommand = new CopyObjectCommand({
-        Bucket: bucket,
-        CopySource: `${bucket}/${oldKey}`,
-        Key: newKey,
-    });
-    const deleteCommand = new DeleteObjectCommand({
-        Bucket: bucket,
-        Key: oldKey,
-    });
+  const copyCommand = new CopyObjectCommand({
+    Bucket: name,
+    CopySource: `${name}/${oldKey}`,
+    Key: newKey,
+  });
+  const deleteCommand = new DeleteObjectCommand({
+    Bucket: name,
+    Key: oldKey,
+  });
 
-    try {
-        const copyResponse = await client.send(copyCommand);
+  try {
+    const copyResponse = await client.send(copyCommand);
 
-        if (copyResponse.$metadata.httpStatusCode === 200) {
-            const deleteResponse = await client.send(deleteCommand);
-            return deleteResponse;
-        } else {
-            return "Failed to rename or move object";
-        }
-    } catch (error) {
-        console.error(error);
-        return "Failed to rename or move object";
+    if (copyResponse.$metadata.httpStatusCode === 200) {
+      const deleteResponse = await client.send(deleteCommand);
+      return deleteResponse;
+    } else {
+      return "Failed to rename or move object";
     }
+  } catch (error) {
+    console.error(error);
+    return "Failed to rename or move object";
+  }
 };
 
-export async function PUT(
-    req: NextRequest,
-  ) {
-    const body = await req.json();
-    const oldKey = body.oldKey;
-    const newKey = body.newKey;
-    const response = await renameOrMoveObject({ oldKey, newKey });
-    
-    return NextResponse.json(response, { status: 200 });
-};
+export async function PUT(req: NextRequest) {
+  const session = await getUserSession(req);
+  const body = await req.json();
+  const oldKey = body.oldKey;
+  const newKey = body.newKey;
+
+  const bucket = await getBucketById(new ObjectId(session.bucketId));
+  if (!bucket) {
+    return NextResponse.json("Bucket not found", { status: 404 });
+  }
+  const response = await renameOrMoveObject({ oldKey, newKey, bucket });
+
+  return NextResponse.json(response, { status: 200 });
+}
